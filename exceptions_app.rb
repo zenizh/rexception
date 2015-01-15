66450eadd61fedad3896f0fe57200b432273c3e2
kami <hiroki.zenigami@gmail.com>
2015-01-15 20:06:38 +0900

    In development

--- exceptions_app.rb
+++ exceptions_app.rb
@@ -1,35 +1,92 @@
-class ExceptionsController < ActionController::Base
-  STATUSES = Rack::Utils::SYMBOL_TO_STATUS_CODE.select { |_, code| code >= 400 }
+require 'singleton'
 
-  STATUSES.each do |status, _|
-    eval <<-RUBY
-      def #{status}
-        render template_exists?(view) ? view : view(:internal_server_error)
-      end
-    RUBY
-  end
-
-  def view(status = status())
-    "errors/#{status}"
-  end
-
-  def status
-    self.class.status(env)
-  end
-
+module ExceptionsApp
   class << self
-    def call(env)
-      action(status(env)).call(env)
+    def setup(&block)
+      Setting.instance.setup(&block)
     end
 
-    def status(env)
-      ActionDispatch::ExceptionWrapper.rescue_responses[exception(env).class.name]
+    def setting
+      Setting.instance
+    end
+  end
+
+  class Setting
+    include Singleton
+
+    attr_accessor :layout
+    attr_accessor :errors_dir
+    attr_accessor :rescue_responses
+
+    def initialize
+      defaults.each { |k, v| instance_variable_set("@#{k}", v) }
     end
 
-    def exception(env)
-      env['action_dispatch.exception']
+    def setup
+      yield self
+    end
+
+    private
+
+    def defaults
+      @defaults ||= {
+        layout:     'application',
+        errors_dir: 'errors',
+        rescue_responses: {}
+      }
+    end
+  end
+
+  class ExceptionsController < ActionController::Base
+    STATUSES = Rack::Utils::SYMBOL_TO_STATUS_CODE.select { |_, code| code >= 400 }
+
+    STATUSES.each do |status, code|
+      eval <<-RUBY
+        def #{status}
+          render template_exists?(view) ? view : view(:internal_server_error), status: #{code}
+        end
+      RUBY
+    end
+
+    layout -> { setting.layout }
+
+    private
+
+    def view(status = status())
+      "#{setting.errors_dir}/#{status}"
+    end
+
+    def status
+      self.class.status(env)
+    end
+
+    def setting
+      ExceptionsApp.setting
+    end
+
+    class << self
+      def call(env)
+        action(status(env)).call(env)
+      end
+
+      def status(env)
+        ActionDispatch::ExceptionWrapper.rescue_responses[exception(env).class.name]
+      end
+
+      def exception(env)
+        env['action_dispatch.exception']
+      end
     end
   end
 end
 
-Rails.application.config.exceptions_app = ExceptionsController
+ExceptionsApp.setup do |config|
+  config.layout = 'application'
+
+  config.errors_dir = 'errors'
+
+  config.rescue_responses = {
+  }
+end
+
+Rails.application.config.exceptions_app = ExceptionsApp::ExceptionsController
